ca205047d809149f0330ab6a8e3437ad439cdb2d
ANTONY MATHEW <antonymathew@ANTONYs-MacBook-Pro.local>
2021-02-16 00:03:54 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,17 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    RestaurantService service = new RestaurantService();
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        LocalTime openingTime = LocalTime.parse("00:00:00");
+        LocalTime closingTime = LocalTime.parse("23:59:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         Boolean isResturentOpen=restaurant.isRestaurantOpen();
         assertTrue(isResturentOpen);
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("10:31:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         Boolean isResturentClose=restaurant.isRestaurantOpen();
         assertFalse(isResturentClose);
     }
